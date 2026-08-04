6f2a36f74070ce5a17af3bad73369e6fd70d81d5
Hasini <boosahasini480@gmail.com>
2026-08-04 13:50:38 +0530

    Add files via upload

---
+++ codespace/Marks.java
@@ -0,0 +1,31 @@
+class Ex1 {
+	private String name;
+	private int marks;
+	
+	public String getName(){
+		return name;
+	}
+	public int getMarks(){
+		return marks;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setMarks(int marks){
+		if(marks >= 0 && marks <= 100){
+			this.marks = marks;
+		}
+		else{
+			System.out.println("Invalid marks");
+		}
+	}
+}
+public class Marks{
+	public static void main(String args[]){
+		Ex1 obj = new Ex1();
+		obj.setName("Hasini");
+		obj.setMarks(95);
+		System.out.println(obj.getName());
+		System.out.println(obj.getMarks());
+	}
+}
